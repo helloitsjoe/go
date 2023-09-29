a0523569a4af539f3ee769bc5cd55ec870c68aaf
Joe Boyle <jboyle617@gmail.com>
2023-09-28 22:58:33 -0400

    hash

--- htmx/handlers/handlers_htmx.go
+++ htmx/handlers/handlers_htmx.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type ctx map[string]interface{}
@@ -23,11 +24,14 @@ type user struct {
 
 type User struct {
 	Username string
+	Id       int
 }
 
 type Handlers struct {
 	db string
 }
+
+var uniqueId = 0
 
 func New(db string) Handlers {
 
@@ -38,7 +42,8 @@ func SeedUsers() {
 	u := [3]string{"Alice", "Bob", "Carl"}
 
 	for _, name := range u {
-		Users[name] = User{name}
+		Users[name] = User{name, uniqueId}
+		users[name] = user{name, "password"}
 	}
 }
 
@@ -51,6 +56,16 @@ func Index(c echo.Context) error {
 	// If auth cookie is valid, return logged in page
 	data := ctx{"Register": "true", "Users": Users}
 	return c.Render(http.StatusOK, "index.html", data)
+}
+
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	return string(bytes), err
+}
+
+func checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 
 func RegisterUser(c echo.Context) error {
@@ -69,10 +84,16 @@ func RegisterUser(c echo.Context) error {
 		return c.Render(http.StatusOK, "error.html", ctx{"Error": "Name and password must be provided"})
 	}
 
-	// TODO: Hash password
+	hashed, err := hashPassword(u.Password)
+	if err != nil {
+		fmt.Println("Error hashing password", err)
+		return c.String(http.StatusInternalServerError, "internal server error")
+	}
+
+	u.Password = hashed
 	users[u.Username] = u
 
-	newUser := User{u.Username}
+	newUser := User{u.Username, uniqueId}
 	Users[u.Username] = newUser
 
 	data := ctx{"User": newUser, "Users": Users}
@@ -82,6 +103,42 @@ func RegisterUser(c echo.Context) error {
 
 func Login(c echo.Context) error {
 	fmt.Println("Body", c.Request().Body)
+	fmt.Println("Password", c.Request().FormValue("password"))
+
+	u := user{}
+
+	if err := c.Bind(&u); err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusBadRequest, "bad request")
+	}
+
+	if u.Username == "" || u.Password == "" {
+		fmt.Println("Name and password must be provided")
+		c.Response().Header().Set("HX-Retarget", "#error")
+		c.Response().Header().Set("HX-Reswap", "innerHTML")
+		return c.Render(http.StatusOK, "error.html", ctx{"Error": "Name and password must be provided"})
+	}
+
+	hashed, err := hashPassword(u.Password)
+	if err != nil {
+		fmt.Println("Error hashing password", err)
+		return c.String(http.StatusInternalServerError, "internal server error")
+	}
+
+	user := users[u.Username]
+	if !checkPasswordHash(user.Password, hashed) {
+		fmt.Println("Incorrect password")
+		c.Response().Header().Set("HX-Retarget", "#error")
+		c.Response().Header().Set("HX-Reswap", "innerHTML")
+		return c.Render(http.StatusOK, "error.html", ctx{"Error": "Incorrect password"})
+	}
+
+	loggedInUser := Users[u.Username]
+
+	data := ctx{"User": loggedInUser, "Users": Users}
+	fmt.Println(data)
+
+	// TODO: Return better response, clear error message
 	return c.HTML(http.StatusOK, "Logged in")
 }
 
